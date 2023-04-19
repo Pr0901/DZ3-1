43443d13ff45daaf46963f23f05b0f842f6a74f4
Sofya <sofa33158@gmail.com>
2023-04-19 20:03:21 +0300

    Test

--- src/test/java/ru/netology/web/CardApplicationTest.java
+++ src/test/java/ru/netology/web/CardApplicationTest.java
@@ -1,10 +1,13 @@
 package ru.netology.web;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.jupiter.api.*;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class CardApplicationTest {
 
@@ -13,7 +16,7 @@ public class CardApplicationTest {
 
     @BeforeAll
     static void setUpAll() {
-        System.setProperty("webdriver.chrome.driver", "./driver/chromedriver.exe");
+        WebDriverManager.chromedriver().setup();
     }
 
     @BeforeEach
@@ -23,6 +26,7 @@ public class CardApplicationTest {
         options.addArguments("--no-sandbox");
         options.addArguments("--headless");
         driver = new ChromeDriver(options);
+        driver.get("http://localhost:9999/");
     }
 
     @AfterEach
@@ -34,14 +38,14 @@ public class CardApplicationTest {
     @Test
     void testPositive() throws InterruptedException {
 
-        driver.get("http://localhost:9999/");
+
         driver.findElement(By.cssSelector("[data-test-id=name] input")).sendKeys("Петр Петров");
-        driver.findElement(By.cssSelector("[data-test-id=phone input")).sendKeys("+79529676386");
+        driver.findElement(By.cssSelector("[data-test-id=phone] input")).sendKeys("+79529676386");
         driver.findElement(By.cssSelector("[data-test-id=agreement]")).click();
         driver.findElement(By.className("button__content")).click();
         String expected = "Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.";
         String actual = driver.findElement(By.cssSelector("[data-test-id=order-success]")).getText();
-        Assertions.assertEquals(expected,actual);
+        assertEquals(expected,actual);
 
     }
 
